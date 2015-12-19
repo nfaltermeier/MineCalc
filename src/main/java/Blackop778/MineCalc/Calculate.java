292f1ce17d96d48a6a1131b0a1de4f1ca77959ad
Blackop778 <nefalt@att.net>
2015-12-18 19:00:38 -0600

    Work on error handling (1.0.1.1)
    WARNING: UNUSABLE

--- src/main/java/Blackop778/MineCalc/Calculate.java
+++ src/main/java/Blackop778/MineCalc/Calculate.java
@@ -9,102 +9,116 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 
-public class Calculate extends CommandBase{
+public class Calculate extends CommandBase {
 
 	@Override
 	public String getCommandName() {
-		//What must be typed in following the / to trigger the command
+		// What must be typed in following the / to trigger the command
 		return "Calculate";
 	}
 
 	@Override
 	public String getCommandUsage(ICommandSender icommandsender) {
-		//What is shown when "/help Calculate" is typed in
+		// What is shown when "/help Calculate" is typed in
 		return "/Calculate <number> <symbol> <number> [symbol] [number]";
 	}
 
 	@Override
 	public void processCommand(ICommandSender icommandsender, String[] arguments) {
-		//What happens when command is entered
+		// What happens when command is entered
 		String print = null;
-		if((arguments.length - 1) % 2 == 0 && arguments.length > 1)
-		{
-			double n = Double.valueOf(arguments[0]);
-			for(int i = 1; i < arguments.length;i++)
-			{
-				if(i + 2 == arguments.length && arguments[i].equals("%"))
-				{
-					print = String.valueOf((int) (n / Double.valueOf(arguments[i + 1]))) + "R" + String.valueOf((int) (n % Double.valueOf(arguments[i + 1])));
-					i++;
-				}
-				else
-				{
-					if(arguments[i].equals("+"))
-					{
-						i++;
-						n = n + Double.valueOf(arguments[i]);
-					}
-					else if(arguments[i].equals("-"))
-					{
-						i++;
-						n = n - Double.valueOf(arguments[i]);
-					}
-					else if(arguments[i].equals("*"))
-					{
-						i++;
-						n = n * Double.valueOf(arguments[i]);
-					}
-					else if(arguments[i].equals("/"))
-					{
-						i++;
-						n = n / Double.valueOf(arguments[i]);
-					}
-					else if(arguments[i].equals("%"))
-					{
-						i++;
-						n = n % Double.valueOf(arguments[i]);
-					}
-					
-					if(i + 1 == arguments.length)
-					{
-						if(n % 1 == 0)
-						{
-							int b = (int) (n);
-							print = String.valueOf(b);
+		boolean divError = false;
+		boolean[] symbolErrors = new boolean[arguments.length];
+		for (int i = 0; i < symbolErrors.length; i++) {
+			symbolErrors[i] = false;
+		}
+		if ((arguments.length - 1) % 2 == 0 && arguments.length > 1) {
+			try {
+				double n = Double.valueOf(arguments[0]);
+				for (int i = 1; i < arguments.length; i++) {
+					if (i + 2 == arguments.length && arguments[i].equals("%")) {
+						if (arguments[i + 1].equals("0")) {
+							divError = true;
 						}
-						else
-						{
-							print = String.valueOf(n);
+						print = String.valueOf((int) (n / Double.valueOf(arguments[i + 1]))) + "R"
+								+ String.valueOf((int) (n % Double.valueOf(arguments[i + 1])));
+						i++;
+					} else {
+						if (arguments[i].equals("+")) {
+							i++;
+							n = n + Double.valueOf(arguments[i]);
+						} else if (arguments[i].equals("-")) {
+							i++;
+							n = n - Double.valueOf(arguments[i]);
+						} else if (arguments[i].equals("*")) {
+							i++;
+							n = n * Double.valueOf(arguments[i]);
+						} else if (arguments[i].equals("/")) {
+							if (arguments[i + 1].equals("0")) {
+								divError = true;
+							}
+							i++;
+							n = n / Double.valueOf(arguments[i]);
+						} else if (arguments[i].equals("%")) {
+							if (arguments[i + 1].equals("0")) {
+								divError = true;
+							}
+							i++;
+							n = n % Double.valueOf(arguments[i]);
+						} else {
+							symbolErrors[i] = true;
+							EntityPlayer player = (EntityPlayer) icommandsender;
+							player.addChatMessage(new ChatComponentText(arguments[i] + "isn't a symbol"));
+							i++;
+						}
+
+						boolean foundError = false;
+
+						if (i + 1 == arguments.length && !foundError) {
+							if (n % 1 == 0) {
+								int b = (int) (n);
+								print = String.valueOf(b);
+							} else {
+								print = String.valueOf(n);
+							}
 						}
 					}
 				}
+			} catch (NumberFormatException error) {
+				print = EnumChatFormatting.RED + "Error: Could not be interpreted as a double:"
+						+ error.getMessage().substring(17, error.getMessage().length());
 			}
+		} else {
+			print = EnumChatFormatting.RED + "Usage: /Calculate <number> <symbol> <number> [symbol] "
+					+ EnumChatFormatting.RED + "[number]";
 		}
-		else
-		{
-			print = EnumChatFormatting.RED+"Usage: /Calculate <number> <symbol> <number> [symbol] [number]";
+
+		if (divError == true && !print.contains("Error")) {
+			print = EnumChatFormatting.RED + "Error: Cannot divide by zero";
 		}
-		if(icommandsender instanceof EntityPlayer) // If the sender was a player
+
+		if (icommandsender instanceof EntityPlayer) // If the sender was a
+													// player
 		{
 			EntityPlayer player = (EntityPlayer) icommandsender;
 			player.addChatMessage(new ChatComponentText(print));
 		}
 	}
-	
+
 	@Override
-	public boolean canCommandSenderUseCommand(ICommandSender icommandsender)
-    {
-        return true;
-    }
-	
+	public boolean canCommandSenderUseCommand(ICommandSender icommandsender) {
+		// User can always use this command
+		return true;
+	}
+
 	@Override
-	public List getCommandAliases()
-    {
+	public List<String> getCommandAliases() {
+		// A list of alternate command names
 		List<String> Aliases = new ArrayList<String>();
 		Aliases.add("calculate");
 		Aliases.add("calc");
 		Aliases.add("Calc");
-        return Aliases;
-    }
-	
+		return Aliases;
+	}
+
 }
--- src/main/java/Blackop778/MineCalc/MineCalc.java
+++ src/main/java/Blackop778/MineCalc/MineCalc.java
@@ -3,7 +3,6 @@ package Blackop778.MineCalc;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
-
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -12,40 +11,37 @@ import net.minecraft.command.ICommandManager;
 import net.minecraft.command.ServerCommandManager;
 import net.minecraft.server.MinecraftServer;
 
-@Mod(modid=MineCalc.MODID, name=MineCalc.MODNAME, version=MineCalc.MODVER, acceptableRemoteVersions="*")
+@Mod(modid = MineCalc.MODID, name = MineCalc.MODNAME, version = MineCalc.MODVER, acceptableRemoteVersions = "*")
 public class MineCalc {
 	public static final String MODID = "minecraftcalculator778";
 	public static final String MODNAME = "MineCalc";
-	public static final String MODVER = "1.0.1.0"; //According to https://mcforge.readthedocs.org/en/latest/conventions/versioning/
-	
+	public static final String MODVER = "1.0.1.1"; // According to
+													// https://mcforge.readthedocs.org/en/latest/conventions/versioning/
+
 	@Instance(value = MineCalc.MODID)
 	public static MineCalc instance;
-	
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event)
-	{
-		
-	}
-	
-	@EventHandler
-    public void load(FMLInitializationEvent event)
-    {
 
-    }
-        
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
-    	
-    }
-    
-    //Called when a server is started, both solo and multiplayer
-    @EventHandler
-    public void serverStart(FMLServerStartingEvent event)
-    {
-    	MinecraftServer server = MinecraftServer.getServer();
-    	ICommandManager command = server.getCommandManager();
-    	ServerCommandManager manager = (ServerCommandManager) command;
-    	manager.registerCommand(new Calculate());
-    }
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void load(FMLInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+
+	}
+
+	// Called when a server is started, both solo and multiplayer
+	@EventHandler
+	public void serverStart(FMLServerStartingEvent event) {
+		MinecraftServer server = MinecraftServer.getServer();
+		ICommandManager command = server.getCommandManager();
+		ServerCommandManager manager = (ServerCommandManager) command;
+		manager.registerCommand(new Calculate());
+	}
 }
