8dadef5a0ef2e51256616f61ffacf1ffcaead29a
Blackop778 <nefalt@att.net>
2016-01-07 22:58:06 -0600

    Kinda started Gui Config
    Not really tho

---
+++ src/main/java/Blackop778/MineCalc/GuiConfig.java
@@ -0,0 +1,8 @@
+package Blackop778.MineCalc;
+
+import net.minecraft.client.gui.GuiScreen;
+
+public class GuiConfig extends GuiScreen
+{
+	// http://jabelarminecraft.blogspot.com/p/minecraft-modding-configuration-guis.html
+}
---
+++ src/main/java/Blackop778/MineCalc/GuiFactoryMineCalc.java
@@ -0,0 +1,40 @@
+package Blackop778.MineCalc;
+
+import java.util.Set;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.IModGuiFactory;
+
+public class GuiFactoryMineCalc implements IModGuiFactory
+{
+
+	@Override
+	public void initialize(Minecraft minecraftInstance)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Class<? extends GuiScreen> mainConfigGuiClass()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
