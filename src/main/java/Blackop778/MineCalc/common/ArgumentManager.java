03f873d70f188495706e075fc1a00fe5fae27f02
Blackop778 <nefalt@att.net>
2016-09-06 19:40:30 -0500

    Compiles

--- src/main/java/Blackop778/MineCalc/common/ArgumentManager.java
+++ src/main/java/Blackop778/MineCalc/common/ArgumentManager.java
@@ -1,53 +1,56 @@
 package Blackop778.MineCalc.common;
 
+import javax.annotation.Nullable;
+
 public class ArgumentManager
 {
 	private String[] args;
-    private int inIndex;
-    private int outIndex;
+	private int inIndex;
 
-    public ArgumentManager()
-    {
-        args = new String[0];
-        inIndex = 0;
-    }
+	public ArgumentManager()
+	{
+		args = new String[0];
+		inIndex = 0;
+	}
 
-    public @Nullable String getAnswer()
-    {
-    	for(int i = 0; i < args.length; i++)
-    	{
-    		if(!args[i].contains("$#"))
-    		{
-    			
-    		}
-    	}
-    }
-    
-    public void addArg(String toAdd)
-    {
-    	args = extendArray(args);
-    	args[inIndex] = toAdd;
-    	inIndex++;
-    }
+	public @Nullable String getAnswer()
+	{
+		for(int i = 0; i < args.length; i++)
+		{
+			if(!args[i].contains("$#"))
+			{
 
-    public static String insertArrayReference(int i)
-    {
-        return "$#" + String.valueOf(i) + "#";
-    }
-    
-    public static <T>[] extendArray(<T>[] array)
-    {
-    	return extendArray(array, 1);
-    }
-    
-    public static <T>[] extendArray(<T>[] array, int lengthToExtend)
-    {
-    	<T>[] newArray = new <T>[array.length + lengthToExtend];
-    	for(int i = 0; i < array.length; i++)
-    	{
-    		newArray[i] = array[i];
-    	}
-    	
-    	return newArray;
-    }
+			}
+		}
+
+		return null;
+	}
+
+	public void addArg(String toAdd)
+	{
+		args = extendArray(args);
+		args[inIndex] = toAdd;
+		inIndex++;
+	}
+
+	public static String insertArrayReference(int i)
+	{
+		return "$#" + String.valueOf(i) + "#";
+	}
+
+	public static String[] extendArray(String[] array)
+	{
+		return extendArray(array, 1);
+	}
+
+	public static String[] extendArray(String[] array, int lengthToExtend)
+	{
+		String[] newArray = new String[array.length + lengthToExtend];
+		for(int i = 0; i < array.length; i++)
+		{
+			newArray[i] = array[i];
+		}
+
+		return newArray;
+	}
 }
