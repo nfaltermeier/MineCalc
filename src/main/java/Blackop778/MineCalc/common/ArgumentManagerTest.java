971f294262464a3624d75ef44c79250872669d4e
Nathan <NSFaltermeier@bluevalleyk12.net>
2016-09-27 07:41:22 -0500

    ugh git why

--- src/main/java/Blackop778/MineCalc/common/ArgumentManagerTest.java
+++ src/main/java/Blackop778/MineCalc/common/ArgumentManagerTest.java
@@ -6,6 +6,7 @@ public class ArgumentManagerTest
 	public static void main(String[] args)
 	{
 		ArgumentManager manager = new ArgumentManager(false);
+		manager.digest("4 +8/3+((4+0)*3)+1");
 		System.exit(0);
 	}
 
