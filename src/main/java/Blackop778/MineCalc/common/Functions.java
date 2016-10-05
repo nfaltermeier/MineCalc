1ebcf30790eabbe2e425978798caa101256704a6
Nathan <NSFaltermeier@bluevalleyk12.net>
2016-10-05 07:39:55 -0500

    Added more functions
    Changed functions to anonymous inner classes

--- src/main/java/Blackop778/MineCalc/common/Functions.java
+++ src/main/java/Blackop778/MineCalc/common/Functions.java
@@ -1,24 +1,103 @@
 package Blackop778.MineCalc.common;
 
+import Blackop778.MineCalc.MineCalc;
 import Blackop778.MineCalc.common.ArgumentManager.Type;
 
 public abstract class Functions
 {
-
-	public static class Addition implements IFunction
+	static void addFunctions()
 	{
-		@Override
-		public Type getType(Character currentChar, Type lastType)
-		{
-			if(currentChar.equals('+'))
-				return Type.ADDITION;
-			return Type.JUNK;
-		}
+		MineCalc.functions.add(new IFunction(){
+			@Override
+			public Type getType(Character currentChar, Type lastType) {
+				if(currentChar.equals('+'))
+					return Type.ADDITION;
+				return Type.JUNK;
+			}
 
-		@Override
-		public double evaluateFunction(double n1, double n2) throws CalcExceptions
-		{
-			return n1 + n2;
-		}
+			@Override
+			public double evaluateFunction(double n1, double n2) throws CalcExceptions {
+				return n1 + n2;
+			}
+		});
+		
+		MineCalc.functions.add(new IFunction(){
+			@Override
+			public Type getType(Character currentChar, Type lastType) {
+				if(currentChar.equals('-'))
+				{
+					if(lastType.equals(Type.DIVISION))
+						return Type.EXPONENTROOT;
+					return Type.DIVISION;
+				}
+					
+				return Type.JUNK;
+			}
+
+			@Override
+			public double evaluateFunction(double n1, double n2) throws CalcExceptions {
+				return n1 - n2;
+			}
+		});
+		
+		MineCalc.functions.add(new IFunction(){
+			@Override
+			public Type getType(Character currentChar, Type lastType) {
+				if(currentChar.equals('*') || currentChar.toString().equalsIgnoreCase("x"))
+					return Type.MULTIPLICATION;
+				return Type.JUNK;
+			}
+
+			@Override
+			public double evaluateFunction(double n1, double n2) throws CalcExceptions {
+				return n1 * n2;
+			}
+		});
+		
+		MineCalc.functions.add(new IFunction(){
+			@Override
+			public Type getType(Character currentChar, Type lastType) {
+				if(currentChar.equals('/'))
+					return Type.DIVISION;
+				return Type.JUNK;
+			}
+
+			@Override
+			public double evaluateFunction(double n1, double n2) throws CalcExceptions {
+				if(n2 == 0)
+					throw new CalcExceptions.DivisionException();
+				return n1 / n2;
+			}
+		});
+		
+		MineCalc.functions.add(new IFunction(){
+			@Override
+			public Type getType(Character currentChar, Type lastType) {
+				if(currentChar.equals('%'))
+					return Type.MODULO;
+				return Type.JUNK;
+			}
+
+			@Override
+			public double evaluateFunction(double n1, double n2) throws CalcExceptions {
+				if(n2 == 0)
+					throw new CalcExceptions.DivisionException();
+				return n1 % n2;
+			}
+		});
+		
+		MineCalc.functions.add(new IFunction(){
+			@Override
+			public Type getType(Character currentChar, Type lastType) {
+				if(currentChar.equals('^'))
+					return Type.EXPONENTROOT;
+				return Type.JUNK;
+			}
+
+			@Override
+			public double evaluateFunction(double n1, double n2) throws CalcExceptions {
+				
+			}
+		});
 	}
 }
