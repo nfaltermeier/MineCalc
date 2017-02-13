73e773ced3f629eb62c4decfdf31e815b5f6365e
Blackop778 <nefalt2@gmail.com>
2017-02-12 23:05:35 -0600

    Guess I forgot to format a couple files

--- src/main/java/Blackop778/MineCalc/client/config/GuiConfigMineCalc.java
+++ src/main/java/Blackop778/MineCalc/client/config/GuiConfigMineCalc.java
@@ -8,36 +8,31 @@ import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.config.GuiConfig;
 
-public class GuiConfigMineCalc extends GuiConfig
-{
-	// http://jabelarminecraft.blogspot.com/p/minecraft-modding-configuration-guis.html
-	public GuiConfigMineCalc(GuiScreen parent)
-	{
-		super(parent,
-				new ConfigElement(MCConfig.getConfig().getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
-				MineCalc.MODID, false, false, "Calculate All the Things",
-				MCConfig.getConfig().getConfigFile().getAbsolutePath());
-	}
+public class GuiConfigMineCalc extends GuiConfig {
+    // http://jabelarminecraft.blogspot.com/p/minecraft-modding-configuration-guis.html
+    public GuiConfigMineCalc(GuiScreen parent) {
+	super(parent,
+		new ConfigElement(MCConfig.getConfig().getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+		MineCalc.MODID, false, false, "Calculate All the Things",
+		MCConfig.getConfig().getConfigFile().getAbsolutePath());
+    }
 
-	@Override
-	public void initGui()
-	{
-		// You can add buttons and initialize fields here
-		super.initGui();
-	}
+    @Override
+    public void initGui() {
+	// You can add buttons and initialize fields here
+	super.initGui();
+    }
 
-	@Override
-	public void drawScreen(int mouseX, int mouseY, float partialTicks)
-	{
-		// You can do things like create animations, draw additional elements,
-		// etc. here
-		super.drawScreen(mouseX, mouseY, partialTicks);
-	}
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+	// You can do things like create animations, draw additional elements,
+	// etc. here
+	super.drawScreen(mouseX, mouseY, partialTicks);
+    }
 
-	@Override
-	protected void actionPerformed(GuiButton button)
-	{
-		// You can process any additional buttons you may have added here
-		super.actionPerformed(button);
-	}
+    @Override
+    protected void actionPerformed(GuiButton button) {
+	// You can process any additional buttons you may have added here
+	super.actionPerformed(button);
+    }
 }
--- src/main/java/Blackop778/MineCalc/client/config/GuiFactoryMineCalc.java
+++ src/main/java/Blackop778/MineCalc/client/config/GuiFactoryMineCalc.java
@@ -6,36 +6,31 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.client.IModGuiFactory;
 
-public class GuiFactoryMineCalc implements IModGuiFactory
-{
+public class GuiFactoryMineCalc implements IModGuiFactory {
 
-	@Override
-	public void initialize(Minecraft minecraftInstance)
-	{
-		// Called when Minecraft finishes loading
+    @Override
+    public void initialize(Minecraft minecraftInstance) {
+	// Called when Minecraft finishes loading
 
-	}
+    }
 
-	@Override
-	public Class<? extends GuiScreen> mainConfigGuiClass()
-	{
-		// Needs a return that extends GuiConfig, which will be the main Gui
-		// Class
-		return GuiConfigMineCalc.class;
-	}
+    @Override
+    public Class<? extends GuiScreen> mainConfigGuiClass() {
+	// Needs a return that extends GuiConfig, which will be the main Gui
+	// Class
+	return GuiConfigMineCalc.class;
+    }
 
-	@Override
-	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories()
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+	// TODO Auto-generated method stub
+	return null;
+    }
 
-	@Override
-	public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element)
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
+	// TODO Auto-generated method stub
+	return null;
+    }
 
 }
