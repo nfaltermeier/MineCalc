25c402f1c9e14e8e07f8ff7fddafde061292984f
Blackop778 <nefalt2@gmail.com>
2017-02-20 02:42:31 -0600

    Double parenthesis works. Hail the helper methods.

--- src/main/java/Blackop778/MineCalc/common/ArgumentManagerTest.java
+++ src/main/java/Blackop778/MineCalc/common/ArgumentManagerTest.java
@@ -1,5 +1,7 @@
 package Blackop778.MineCalc.common;
 
+import Blackop778.MineCalc.common.Calculator.AllStandinsUsedException;
+
 public abstract class ArgumentManagerTest {
     @SuppressWarnings("unused")
     public static void main(String[] args) {
@@ -11,6 +13,8 @@ public abstract class ArgumentManagerTest {
 		System.out.println(Calculator.evaluate("5%3*9", true));
 		System.out.println(Calculator.evaluate("6*6/--2+((-1*1)+1+(1*-1))*3", false));
 	    } catch (CalcExceptions e) {
+		e.printStackTrace();
+	    } catch (AllStandinsUsedException e) {
 		e.printStackTrace();
 	    }
 	}
@@ -26,6 +30,9 @@ public abstract class ArgumentManagerTest {
 	    System.out.println("Expected: -5 Recieved: " + Calculator.evaluate("5*(1+1-(6/2))", true));
 	    System.out.println("Expected: 11  Recieved: " + Calculator.evaluate("5--6", false));
 	} catch (CalcExceptions e) {
+	    e.printStackTrace();
+	} catch (AllStandinsUsedException e) {
+	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
     }
--- src/main/java/Blackop778/MineCalc/common/Calculator.java
+++ src/main/java/Blackop778/MineCalc/common/Calculator.java
@@ -1,14 +1,17 @@
 package Blackop778.MineCalc.common;
 
 import java.util.Stack;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import Blackop778.MineCalc.MineCalc;
 
 public abstract class Calculator {
+    public static final Character[] standInChars = { '$', '#', '@', '"', ';', ':', '?', '&', '[', '{', ']', '}', '|',
+	    '!' };
     public static final Argument SORTING_HAT = new Argument(0, 0, "f");
 
-    public static double evaluate(String math, boolean useOOPS) throws CalcExceptions {
+    public static double evaluate(String math, boolean useOOPS) throws CalcExceptions, AllStandinsUsedException {
 	math = math.replaceAll("\\s", "");
 	ArgumentManager arguments = new ArgumentManager();
 	Stack<Integer> parenthesisStartIndex = new Stack<Integer>();
@@ -85,14 +88,19 @@ public abstract class Calculator {
 	    if (contents.startsWith("(") && contents.endsWith(")")) {
 		contents = contents.substring(1, contents.length() - 1);
 	    }
-	    String replacer;
-	    if (op.equals("-")) {
+	    Character replacer = findUnusedStandin(contents);
+	    if (operator.equals("-")) {
 		String[] temp = takeMinuses(contents, index);
 		contents = temp[0];
-		replacer = temp[1];
+		replacer = temp[1].charAt(0);
 	    }
-	    String trimmedContents = trimToOperation(contents, operator, index - 1);
+	    String trimmedContents = trimToOperation(contents, operator, index - 1, replacer);
 	    String[] numbersS = trimmedContents.split(Pattern.quote(operator));
+	    if (operator.equals("-")) {
+		numbersS[0] = addMinus(numbersS[0], replacer);
+		numbersS[1] = addMinus(numbersS[1], replacer);
+		trimmedContents = addMinus(trimmedContents, replacer);
+	    }
 	    double[] numbers = { Double.valueOf(numbersS[0]), Double.valueOf(numbersS[1]) };
 	    double answer = op.evaluateFunction(numbers[0], numbers[1]);
 	    if (arguments.get(0).contents.equals("(" + trimmedContents + ")")) {
@@ -106,7 +114,13 @@ public abstract class Calculator {
 	return Double.valueOf(arguments.get(0).contents);
     }
 
-    public static String trimToOperation(String math, String operationSymbol, int symbolStartIndex) {
+    public static String trimToOperation(String math, String operationSymbol, int symbolStartIndex)
+	    throws AllStandinsUsedException {
+	return trimToOperation(math, operationSymbol, symbolStartIndex, findUnusedStandin(math));
+    }
+
+    public static String trimToOperation(String math, String operationSymbol, int symbolStartIndex,
+	    Character numberStandin) throws AllStandinsUsedException {
 	int index = 0;
 	int lastIndex = 0;
 	// Eliminate other instances of operationSymbol
@@ -122,7 +136,8 @@ public abstract class Calculator {
 	String math1 = maths[0];
 	lastIndex = math1.length();
 	for (index = lastIndex - 1; index > -1; index--) {
-	    if (!isNumber(math1.charAt(index), tryCharAt(math1, index - 1), tryCharAt(math1, index - 2))) {
+	    if (!isNumber(math1.charAt(index), tryCharAt(math1, index - 1), tryCharAt(math1, index - 2),
+		    numberStandin)) {
 		index++;
 		break;
 	    }
@@ -137,7 +152,8 @@ public abstract class Calculator {
 	String math2 = maths[1];
 	lastIndex = 0;
 	for (index = lastIndex; index < math2.length(); index++) {
-	    if (!isNumber(math2.charAt(index), tryCharAt(math2, index - 1), tryCharAt(math2, index - 2))) {
+	    if (!isNumber(math2.charAt(index), tryCharAt(math2, index - 1), tryCharAt(math2, index - 2),
+		    numberStandin)) {
 		break;
 	    }
 	}
@@ -159,17 +175,55 @@ public abstract class Calculator {
 	}
     }
 
-    public static boolean isNumber(Character current, Character last, Character lastLast) {
-	if (current.toString().matches("\\d|\\.|[lpiLPI]"))
+    public static String concatNullableCharacters(Character first, Character... others) {
+	StringBuilder toReturn = new StringBuilder();
+	if (first != null)
+	    toReturn.append(first);
+	for (Character c : others) {
+	    if (c != null)
+		toReturn.append(c);
+	}
+
+	return toReturn.toString();
+    }
+
+    public static boolean isNumber(Character current, Character last, Character lastLast)
+	    throws AllStandinsUsedException {
+	return isNumber(current, last, lastLast, findUnusedStandin(concatNullableCharacters(current, last, lastLast)));
+    }
+
+    public static boolean isNumber(Character current, Character last, Character lastLast, Character numberStandIn) {
+	if (current.toString().matches("\\d|\\.|[lpiLPI]|" + Pattern.quote(numberStandIn.toString())))
 	    return true;
 	else if (current.equals('-') && !(new Character('/').equals(lastLast))) {
 	    if (last == null)
 		return true;
-	    else if (!last.toString().matches("\\d|\\.|[lpiLPI]"))
+	    else if (!last.toString().matches("\\d|\\.|[lpiLPI]|" + Pattern.quote(numberStandIn.toString())))
 		return true;
 	}
 
 	return false;
+    }
+
+    public static Character findUnusedStandin(String text) throws AllStandinsUsedException {
+	for (Character c : standInChars) {
+	    if (!text.contains(new StringBuilder().append(c).toString()))
+		return c;
+	}
+
+	StringBuilder chars = new StringBuilder();
+	for (Character c : standInChars) {
+	    chars.append("'").append(c).append("', ");
+	}
+	throw new AllStandinsUsedException(chars.toString().substring(0, chars.toString().length() - 2));
+    }
+
+    public static class AllStandinsUsedException extends Exception {
+	private static final long serialVersionUID = 682745941841678365L;
+
+	public AllStandinsUsedException(String standins) {
+	    super(standins);
+	}
     }
 
     /**
@@ -178,25 +232,19 @@ public abstract class Calculator {
      * @param unchangedIndex
      * @return the changed string followed by the char the extra minus signs
      *         were replaced by
+     * @throws AllStandinsUsedException
      */
-    public static String[] takeMinuses(String toChange, int unchangedIndex) {
-	String[] potentialReplacements = { "$", "#", "@", "'", ";", ":", "?", "&", "[", "{", "]", "}", "|" };
-	String current = "";
-	for (int i = 0; i < potentialReplacements.length; i++) {
-	    current = potentialReplacements[i];
-	    if (toChange.contains(current)) {
-		break;
-	    }
-	}
+    public static String[] takeMinuses(String toChange, int unchangedIndex) throws AllStandinsUsedException {
+	Character replacement = findUnusedStandin(toChange);
 
-	toChange = toChange.replaceAll(Pattern.quote("-"), current);
-	toChange = toChange.substring(0, unchangedIndex) + "-"
-		+ toChange.substring(unchangedIndex + 1, toChange.length());
+	toChange = toChange.replaceAll(Pattern.quote("-"), Matcher.quoteReplacement(replacement.toString()));
+	toChange = toChange.substring(0, unchangedIndex - 1) + "-"
+		+ toChange.substring(unchangedIndex, toChange.length());
 
-	return new String[] { toChange, current };
+	return new String[] { toChange, replacement.toString() };
     }
 
-    public static String addMinus(String math, String minusReplacer) {
-	return math.replaceAll(Pattern.quote(minusReplacer), "-");
+    public static String addMinus(String math, Character minusReplacer) {
+	return math.replaceAll(Pattern.quote(minusReplacer.toString()), "-");
     }
 }
